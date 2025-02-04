4331bb3e5205ba11daae883d0c3014677ea83fff
salvijusbieliauskas <salvijusbagdzevicius@gmail.com>
2025-02-04 15:22:09 +0200

    Modified starting project template

--- Parduotuve/Program.cs
+++ Parduotuve/Program.cs
@@ -1,21 +1,15 @@
-using Parduotuve.Client.Pages;
 using Parduotuve.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseWebAssemblyDebugging();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -28,8 +22,6 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(Parduotuve.Client._Imports).Assembly);
+    .AddInteractiveServerRenderMode();
 
 app.Run();
