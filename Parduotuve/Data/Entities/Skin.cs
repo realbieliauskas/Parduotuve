a5c98204a18708ac0c0dd53a1a60597d75b1c63f
Knygneshys <lukasmieliauskas@gmail.com>
2025-03-14 10:18:41 +0200

    Added "Add skin" button to the AdminControlPanelPage and setup its logic. Now it is possible for the admin to dynamically add new skins. Still need to add the ability to add chromas

--- Parduotuve/Data/Entities/Skin.cs
+++ Parduotuve/Data/Entities/Skin.cs
@@ -5,7 +5,6 @@ namespace Parduotuve.Data.Entities
     public class Skin
     {
         public int Id { get; set; }
-
         public string? Name { get; set; }
 
         public string? ChampionName { get; set; }
