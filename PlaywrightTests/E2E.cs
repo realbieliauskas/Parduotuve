0a16a9d3bd7fde21cf9d37f2f4285c458c21f664
Knygneshys <lukasmieliauskas@gmail.com>
2025-05-10 18:24:40 +0300

    Setup .runsettings file for easier debugging. Setup second page that tests admin loggin.

--- PlaywrightTests/E2E.cs
+++ PlaywrightTests/E2E.cs
@@ -5,31 +5,34 @@ using Microsoft.Playwright.Xunit;
 
 namespace PlaywrightTests;
 
-public class E2E : PageTest
+public class E2E : BlazorPageTest
 {
+    private string pageUrl = $"{RootUri.AbsoluteUri}";
+
     [Fact]
-    public async Task LoginButtonTakesYouFromBrowsePageToLoginPage()
+    public async Task LoginButton_TakesYou_From_BrowsePage_To_LoginPage()
     {
-        await Page.GotoAsync("https://reallybad.tech/");
+        await Page.GotoAsync(pageUrl);
 
         await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
 
-        await Expect(Page).ToHaveURLAsync("https://reallybad.tech/Login");
+        await Expect(Page).ToHaveURLAsync($"{pageUrl}Login");
     }
 
     [Fact]
     public async Task LoginAsAdmin()
     {
-        await Page.GotoAsync("https://reallybad.tech/Login");
+        await Page.PauseAsync();
+        await Page.GotoAsync($"{pageUrl}Login");
 
         await Page.GetByLabel("Username").FillAsync("alv");
         await Page.GetByLabel("Password").FillAsync("crz");
 
-        await Page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
 
-        await Expect(Page).ToHaveURLAsync("https://reallybad.tech/AdminPanel");
+        await Expect(Page).ToHaveURLAsync($"{pageUrl}AdminPanel");
         await Page.GetByText("Greetings, administrator!").IsVisibleAsync();
 
-        //await Page.GetByRole(AriaRole.Link, new() { Name = "Admin dashboard" }).ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Admin dashboard" }).ClickAsync();
     }
 }
