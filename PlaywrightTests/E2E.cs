8f8f150b55e27bda123c55e1de00d73b272945c3
Knygneshys <lukasmieliauskas@gmail.com>
2025-05-10 19:57:09 +0300

    Added sorting test

--- PlaywrightTests/E2E.cs
+++ PlaywrightTests/E2E.cs
@@ -59,8 +59,31 @@ public class E2E : BlazorPageTest
     }
 
     [Fact]
-    public async Task RegisterUser_CheckIfUserHasWelcomeMessage()
+    public async Task SortSkinsByPrice_Then_SortThemByPriceInReverseOrder()
     {
-
+        // await Page.PauseAsync();
+        await Page.GotoAsync(pageUrl);
+        await Page.GetByText("Champion Name").ClickAsync();
+        await Page.GetByText("Price").ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Goth Annie" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Red Riding Annie" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Prom Queen Annie" })).ToBeVisibleAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Frostfire Annie" })).ToBeVisibleAsync();
+        await Expect(Page.Locator("h6").First).ToBeVisibleAsync();
+        await Expect(Page.Locator("div:nth-child(2) > .mud-card-actions > .mud-typography")).ToBeVisibleAsync();
+        await Expect(Page.Locator("body")).ToContainTextAsync("9.99 €");
+        await Expect(Page.Locator("body")).ToContainTextAsync("9.99 €");
+        await Expect(Page.Locator("body")).ToContainTextAsync("9.99 €");
+        await Expect(Page.Locator("body")).ToContainTextAsync("9.99 €");
+        await Page.GetByRole(AriaRole.Checkbox, new() { Name = "Reverse sort" }).CheckAsync();
+        await Page.GotoAsync($"{pageUrl}?SortBy=Price&Page=1&Reverse=True&SearchQuery=");
+        await Expect(Page.Locator(".mud-image").First).ToBeVisibleAsync();
+        await Expect(Page.Locator("div:nth-child(2) > .mud-image")).ToBeVisibleAsync();
+        await Expect(Page.Locator("div:nth-child(3) > .mud-image")).ToBeVisibleAsync();
+        await Expect(Page.Locator("div:nth-child(4) > .mud-image")).ToBeVisibleAsync();
+        await Expect(Page.Locator("body")).ToContainTextAsync("99.99 €");
+        await Expect(Page.Locator("body")).ToContainTextAsync("99.99 €");
+        await Expect(Page.Locator("body")).ToContainTextAsync("99.99 €");
+        await Expect(Page.Locator("body")).ToContainTextAsync("99.99 €");
     }
 }
